11442d1f60b48cdfd288bb190abdbc11c9c64778
AJ ONeal <coolaj86@gmail.com>
2019-11-01 14:34:30 -0600

    fix

--- publish.sh
+++ publish.sh
@@ -10,7 +10,7 @@ git pull origin master
 git checkout npm
 git checkout master -- package.json
 git checkout master -- README.md
-sed -i '' -e 's|"name": ".root.greenlock"|"name": "greenlock"|' package.json
+sed -i '' -e 's|"name": ".root.greenlock-express"|"name": "greenlock-express"|' package.json
 npm install --save @root/greenlock-express@latest
 git add package* README.md || true
 git commit -m "bump" || true
